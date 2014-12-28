1f2e6237c4544c6e3bcbf9f40298a83460f2c7a6
Octowl <alsaff1987@gmail.com>
2014-12-28 20:54:49 +0300

    created image gallery of all forms in /imgs/

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 /*global $, console */
 
+$.ajax({
+    url: "img",
+    success: function (data) {
+        "use strict";
+        
+        var images = $(data).find("a:contains(.png)");
+        images.each(function () {
+            
+            var $imageAnchor = $(this),
+                imageLink = $imageAnchor.attr("href"),
+                $image = $("<img>").attr('src', imageLink),
+                $li = $("<li></li>");
+            
+            $imageAnchor.text("");
+            $imageAnchor.append($image).append("<p>hi</p>");
+            $li.append($imageAnchor);
+            $("#gallery").append($li);
+        });
+    }
+});
