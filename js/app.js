ee3404e1acfde537fe5ba258559fd727ef66bd61
Octowl <alsaff1987@gmail.com>
2015-01-17 08:18:34 +0300

    added css for a lightbox style overlay

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ $.ajax({
     url: "img",
     success: function (data) {
         "use strict";
-        
+
         var images = $(data).find("a:contains(.png)");
         images.each(function () {
-            
+
             var $imageAnchor = $(this),
                 imageLink = $imageAnchor.attr("href"),
                 $image = $("<img>").attr('src', imageLink),
                 $caption = $("<p></p>").text(imageLink),
                 $li = $("<li></li>");
-            
+
             $imageAnchor.text("");
             $imageAnchor.append($image).append($caption);
             $li.append($imageAnchor);
