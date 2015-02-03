0ee3b48306b3ada41ba6485db1bb9f8ccd719167
Octowl <alsaff1987@gmail.com>
2015-02-03 19:19:59 +0300

    image gallery with overlay

--- js/app.js
+++ js/app.js
@@ -1,23 +1,43 @@
 /*global $, console */
 
-$.ajax({
-    url: "img",
-    success: function (data) {
-        "use strict";
-        
-        var images = $(data).find("a:contains(.png)");
-        images.each(function () {
-            
-            var $imageAnchor = $(this),
-                imageLink = $imageAnchor.attr("href"),
-                $image = $("<img>").attr('src', imageLink),
-                $caption = $("<p></p>").text(imageLink),
-                $li = $("<li></li>");
-            
-            $imageAnchor.text("");
-            $imageAnchor.append($image).append($caption);
-            $li.append($imageAnchor);
-            $("#gallery").append($li);
-        });
-    }
+//Problem: User when clicking on image, goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+//Add an image to overlay
+$overlay.append($image);
+
+//Add a caption to overlay
+$overlay.append($caption);
+
+//Add overlay
+$("body").append($overlay);
+
+//Capture the click event on a link to an image
+$("#gallery a").click(function (event) {
+    "use strict";
+    
+    event.preventDefault();
+    
+    var imageLocation = $(this).attr("href"),
+        captionText = $(this).children("img").attr("alt");
+    
+    //Update overlay with the image from the link
+    $image.attr("src", imageLocation);
+    
+    //Set caption
+    $caption.text(captionText);
+    
+    //Show the overlay
+    $overlay.show();
+});
+
+//When overlay is clicked
+$overlay.click(function () {
+    "use strict";
+    //Hide the overlay
+    $overlay.hide();
 });
