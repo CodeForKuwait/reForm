20a4065d19e1e7483c90f67b850e7d8994907ec3
Octowl <alsaff1987@gmail.com>
2015-02-11 11:32:10 +0300

    overlay has been converted from static image to interactive canvas

--- js/app.js
+++ js/app.js
@@ -4,13 +4,22 @@
 //Solution: Create an overlay with the large image - Lightbox
 
 var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
+//var $image = $("<img>");
+var $canvas = $('<canvas id="formCanvas" width="2480" height="3508"></canvas>')
 var $caption = $("<p></p>");
 var $closeButton = $('<a href="#" id="close" class="myButton">close</a>')
 var $saveButton = $('<a href="#" id="save" class="myButton">save</a>')
 
-//Add an image to overlay
-$overlay.append($image);
+var ctx = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+
+////Add an hidden image to overlay
+//$overlay.append($image);
+
+//Add canvas to overlay
+$overlay.append($canvas);
 
 //Add a caption to overlay
 $overlay.append($caption);
@@ -34,18 +43,46 @@ $("#gallery a").click(function (event) {
         captionText = $(this).children("img").attr("alt");
     
     //Update overlay with the image from the link
-    $image.attr("src", imageLocation);
+//    $image.attr("src", imageLocation);
+    var image = new Image();
+    image.src = imageLocation;
     
     //Set caption
     $caption.text(captionText);
     
+    //Draw the image on the canvas
+    ctx.drawImage(image, 0, 0);
+  
     //Show the overlay
     $overlay.show();
 });
 
-//When overlay is clicked
+//When close button is clicked
 $closeButton.click(function () {
     "use strict";
     //Hide the overlay
     $overlay.hide();
+});
+
+//On mouse events on the canvas
+$canvas.mousedown(function (e) {
+    "use strict";
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function (e) {
+    "use strict";
+    //draw lines
+    if (mouseDown) {
+        ctx.beginPath();
+        ctx.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function () {
+    "use strict";
+    mouseDown = false;
+}).mouseleave(function () {
+    $canvas.mouseup();
 });
