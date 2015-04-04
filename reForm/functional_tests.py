778473f94085fee68783a55ad7153369c10f3f1a
Octowl <alsaff1987@gmail.com>
2015-04-04 12:14:08 +0300

    first functional test commented. Switched to using unittest

--- reForm/functional_tests.py
+++ reForm/functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        # Fulan closes his browser
+        self.browser.quit()
+
+    def test_can_start_a_form_and_retrieve_it_later(self):
+        # Fulan has heard about an amazing service for filling out government forms.
+        # He goes to check out the homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and the header say "reForm"
+        self.assertIn('reForm', self.browser.title)
+
+        self.fail("Finish the test!")
+
+
+if __name__ == '__main__':
+    unittest.main()
